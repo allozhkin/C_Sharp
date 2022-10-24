6f7d67fb72c09b51faca087fecee512149d15bdc
alexey <lozhkin023@gmail.com>
2022-10-24 16:13:24 +0300

    done hw7 ex001 Написать программу, упорядочивания по убыванию элементы каждой строки двумерной массива

--- HW_seminar_7/Ex_001/Program.cs
+++ HW_seminar_7/Ex_001/Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, упорядочивания по убыванию элементы каждой строки двумерной массива.
+
+void FillMatrix(int[,] array)
+{
+    Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = random.Next(0, 10);
+        }
+    }
+}
+void PrintMatrix(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j]);
+        }
+        Console.WriteLine();
+    }
+}
+void SelectionSort(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(1); k++)
+            {
+                if (array[i, j] >= array[i, k])
+                {
+                    int temporary = array[i, j];
+                    array[i, j] = array[i, k];
+                    array[i, k] = temporary;
+                }
+            }
+        }
+    }
+
+}
+Console.WriteLine("Введите колличество строк: ");
+int stroki = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите колличество столбов: ");
+int stolb = int.Parse(Console.ReadLine() ?? "0");
+int[,] array = new int[stroki, stolb];
+FillMatrix(array);
+PrintMatrix(array);
+Console.WriteLine();
+SelectionSort(array);
+PrintMatrix(array);
--- lesson_0.3_09.10.22/ex_004/Program.cs
+++ lesson_0.3_09.10.22/ex_004/Program.cs
@@ -1,6 +1,6 @@
-﻿int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+﻿int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
 
-void PrintArray(int[]array)
+void PrintArray(int[] array)
 {
     int count = array.Length;
     for (int i = 0; i < count; i++)
@@ -12,11 +12,11 @@ void PrintArray(int[]array)
 
 void SelectionSort(int[] array)
 {
-    
-    for (int i = 0; i < array.Length; i++)
+
+    for (int i = 0; i < array.Length - 1; i++)
     {
         int minPosition = i;
-        for ( int j = i + 1; j < array.Length-1; j++)
+        for (int j = i + 1; j < array.Length; j++)
         {
             if (array[j] < array[minPosition]) minPosition = j;
         }
