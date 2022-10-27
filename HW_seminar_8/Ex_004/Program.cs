4506b4daf16b3aba0a4230dcf006f889b1ee96a5
alexey <lozhkin023@gmail.com>
2022-10-27 21:27:03 +0300

    done Показать треугольник Паскаля

---
+++ HW_seminar_8/Ex_004/Program.cs
@@ -0,0 +1,34 @@
+﻿// Показать треугольник Паскаля *Сделать вывод в виде равнобедренного треугольника
+
+
+
+float Factorial(int n)
+{
+    float i, x = 1;
+    for (i = 1; i < n; i++)
+    {
+        x *= i;
+    }
+    return x;
+}
+
+
+Console.WriteLine("ВВедите количество строк треугольника: ");
+int rows = int.Parse(Console.ReadLine() ?? "0");
+
+
+for (int i = 0; i < rows; i++)
+{
+    for (int c = 0; c <= (rows - i); c++)
+    {
+        Console.Write(" ");
+    }
+    for (int c = 0; c <= i; c++)
+    {
+        Console.Write(" ");
+        Console.Write(Factorial(i) / (Factorial(c) * Factorial(i - c)));
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+}
+Console.WriteLine();
