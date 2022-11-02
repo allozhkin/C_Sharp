fb8c8fa07ead8890605c25cbf28f01a812ccb62e
alexey <lozhkin023@gmail.com>
2022-11-02 22:54:34 +0300

    done

--- seminar_0.8_23.10.22/Ex_001/Program.cs
+++ seminar_0.8_23.10.22/Ex_001/Program.cs
@@ -1,34 +1,65 @@
 ﻿
 
-void FillMatrix(int[,] array)
+void FillArray(int[] array)
 {
     Random random = new Random();
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = random.Next(0, 9);
-        }
+        array[i] = random.Next(0, 9);
     }
 }
-void PrintMatrix(int[,] array)
+void PrintArray(int[] array)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write($"{array[i, j]} ");
-        }
-        Console.WriteLine();
+        Console.Write($"{array[i]} ");
     }
 }
 
-Console.WriteLine("Введите колличество строк: ");
-int stroki = int.Parse(Console.ReadLine() ?? "0");
-Console.WriteLine("Введите колличество столбов: ");
-int stolbi = int.Parse(Console.ReadLine() ?? "0");
 
-// string text = "Я люблю свою лошадку, Причешу ей шёрстку гладко, Гребешком приглажу хвостик, верхом поеду в гости.";
-int[,] array = new int[stroki, stolbi];
-FillMatrix(array);
-PrintMatrix(array);
+Console.WriteLine("Введите длинну массива: ");
+int size = int.Parse(Console.ReadLine() ?? "0");
+int[] array = new int[size];
+Console.WriteLine();
+FillArray(array);
+PrintArray(array);
+
+System.Console.WriteLine();
+
+
+int zero = 0, one = 0, two = 0, three = 0, four = 0, five = 0, six = 0, seven = 0, eight = 0, nine = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == 0)
+        zero++;
+        
+    else if (array[i] == 1)
+        one++;
+    else if (array[i] == 2)
+        two++;
+    else if (array[i] == 3)
+        three++;
+    else if (array[i] == 4)
+        four++;
+    else if (array[i] == 5)
+        five++;
+    else if (array[i] == 6)
+        six++;
+    else if (array[i] == 7)
+        seven++;
+    else if (array[i] == 8)
+        eight++;
+    else if (array[i] == 9)
+        nine++;
+}
+Console.WriteLine($"0 встречается {zero} раз");
+Console.WriteLine($"1 встречается {one} раз");
+Console.WriteLine($"2 встречается {two} раз");
+Console.WriteLine($"3 встречается {three} раз");
+Console.WriteLine($"4 встречается {four} раз");
+Console.WriteLine($"5 встречается {five} раз");
+Console.WriteLine($"6 встречается {six} раз");
+Console.WriteLine($"7 встречается {seven} раз");
+Console.WriteLine($"8 встречается {eight} раз");
+Console.WriteLine($"9 встречается {nine} раз");
