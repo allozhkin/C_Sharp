6297857e4e17cb14fdd73527f63999c2f6a2389e
alexey <lozhkin023@gmail.com>
2022-11-12 14:43:51 +0300

    Показать натуральные числа от N до 1, N задано

--- seminar_0.9_12.11.22/Ex_001/Program.cs
+++ seminar_0.9_12.11.22/Ex_001/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Показать натуральные числа от 1 до N, N задано
+
+void Num(int number, int num)
+{
+    if (number <= num)
+    {
+        Console.Write(number + "");
+        Num(number + 1, num);
+    }
+}
+Console.WriteLine("введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+Num(1, number);
