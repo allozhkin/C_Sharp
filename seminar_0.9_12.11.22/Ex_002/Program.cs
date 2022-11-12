3cc7d70df99376b657b85b4337f60e60ad6ef0d5
alexey <lozhkin023@gmail.com>
2022-11-12 14:44:28 +0300

    Показать натуральные числа от N до 1, N задано

---
+++ seminar_0.9_12.11.22/Ex_002/Program.cs
@@ -0,0 +1,14 @@
+﻿// Показать натуральные числа от N до 1, N задано
+
+void Num(int number1, int number2)
+{
+    if (number1 >= number2)
+    {
+        Console.Write($"{number1} ");
+        Num(number1 - 1, number2);
+    }
+}
+
+Console.WriteLine("введите число: ");
+int number = int.Parse(Console.ReadLine() ?? "0");
+Num(number, 1);
