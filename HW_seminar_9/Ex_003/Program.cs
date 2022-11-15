997d2532ef4137f2191207b347a2727a31a7eb72
[Alexey] <[lozhkin023@gmail.com]>
2022-11-15 16:13:35 +0300

    done ex003

--- HW_seminar_9/Ex_003/Program.cs
+++ HW_seminar_9/Ex_003/Program.cs
@@ -1,1 +1,14 @@
 ﻿// Написать программу вычисления функции Аккермана
+int Ackermann(int M, int N)
+{
+    if (M == 0) return N + 1;
+    if (M > 0 && N == 0) return Ackermann(M - 1, 1);
+    if (M > 0 && N > 0) return Ackermann(M - 1, Ackermann(M, N - 1));
+    return Ackermann(M, N);
+}
+Console.Write("Введите первое число: ");
+int first = int.Parse(Console.ReadLine() ?? "0");
+Console.Write("Введите второе число: ");
+int second = int.Parse(Console.ReadLine() ?? "0");
+int result = Ackermann(first, second);
+Console.WriteLine($"Функция Аккермана для чисел {first},{second} = {result}");
