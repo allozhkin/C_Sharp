6c536a51aef2d63676f71b5a918f0e4ff717c34d
[Alexey] <[lozhkin023@gmail.com]>
2022-11-15 15:57:25 +0300

    done ex004

---
+++ HW_seminar_9/Ex_004/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих.
+// Первые два элемента последовательности задаются пользователем
+void ShowNumbers(int first, int second, int i, int longg)
+{
+    if (i <= longg)
+    {
+        int sum = first + second;
+        Console.Write($"{sum} ");
+        ShowNumbers(second, sum, i + 1, longg);
+    }
+}
+
+Console.WriteLine("введите число: ");
+int firstNumber = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите число: ");
+int secondNumber = int.Parse(Console.ReadLine() ?? "0");
+
+ShowNumbers(firstNumber, secondNumber, 1, 10);
